e36841401676bd13baac74bebf0805d0ffda6203
irebm2 <irebazaga@gmail.com>
2023-11-17 20:15:00 +0000

    ok

--- script.js
+++ script.js
@@ -89,67 +89,87 @@ var upperCasedCharacters = [
 ];
 
 
+// Function for getting a random element from an array
+function getRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-  let PassNum = ('Would you like your password to have numerical characters?')
-  let PassUpper = ('Would you like your password to have upper case characters?')
-  let PassLower = ('Would you like your password to have lower case characters?')
-  let PassSpecialChar = ('Would you like your password to have special characters?')
-
+  function yesOrNo(question) {
+    let answer;
+    do {
+      answer = prompt(question + " Yes or No?");
+      if (answer.toLowerCase() !== "yes" && answer.toLowerCase() !== "no") {
+        alert("Please enter 'yes' or 'no'");
+      }
+    } while (answer.toLowerCase() !== "yes" && answer.toLowerCase() !== "no");
+    return answer.toLowerCase() === "yes";
+  }
 
   function getValidPasswordLength() {
     let passwordLength;
-  
     do {
       passwordLength = prompt('Enter password length. At least 8 characters but no more than 128.');
-  
+
       if (passwordLength < 8 || passwordLength > 128) {
         alert('Your password does not meet the criteria. It must be between 8 and 128 characters in length.');
       }
     } while (passwordLength < 8 || passwordLength > 128);
-  
     return passwordLength;
   }
+
+  const passLength = getValidPasswordLength();
+  const includeNumbers = yesOrNo('Would you like your password to have numerical characters?');
+  const includeUpper = yesOrNo('Would you like your password to have upper case characters?');
+  const includeLower = yesOrNo('Would you like your password to have lower case characters?');
+  const includeSpecial = yesOrNo('Would you like your password to have special characters?');
   
-  const passwordLength = getValidPasswordLength();
-
-  function yesOrNo(a) {
-    let b;
-    do {
-      b = prompt(a);
-      if (b !== "yes" && b !== "no") {
-        alert("Please enter 'yes' or 'no'");
-      }
-    } while (b !== "yes" && b !== "no");
-
-    if (b === "yes") {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  let PassNumAnswer = yesOrNo(PassNum);
-  let PassUpperAnswer = yesOrNo(PassUpper);
-  let PassLowerAnswer = yesOrNo(PassLower);
-  let PassSpecialCharAnswer = yesOrNo(PassSpecialChar);
-
-}
-
-
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-  return arr[Math.floor(Math.arr() * arr.length)];
-
+  return {
+    passLength,
+    includeNumbers,
+    includeUpper,
+    includeLower,
+    includeSpecial
+  };
 }
 
 // Function to generate password with user input
-function generatePassword(Pass) {
+function generatePasswordWithUserInput() {
+  let options = getPasswordOptions();
 
+  let possibleCharacters = [];
+  let guaranteedCharacters = [];
 
+  if (options.includeNumbers) {
+    possibleCharacters = possibleCharacters.concat(numericCharacters);
+    guaranteedCharacters.push(getRandom(numericCharacters));
+  }
+  
+  if (options.includeUpper) {
+    possibleCharacters = possibleCharacters.concat(upperCasedCharacters);
+    guaranteedCharacters.push(getRandom(upperCasedCharacters));
+  }
+
+  if (options.includeLower) {
+    possibleCharacters = possibleCharacters.concat(lowerCasedCharacters);
+    guaranteedCharacters.push(getRandom(lowerCasedCharacters));
+  }
+
+  if (options.includeSpecial) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    guaranteedCharacters.push(getRandom(specialCharacters));
+  }
+
+  for (let i = 0; i < options.passLength - guaranteedCharacters.length; i++) {
+    guaranteedCharacters.push(getRandom(possibleCharacters));
+  }
+  
+  return guaranteedCharacters.join('');
 }
+
+// Testing our function
+console.log(generatePasswordWithUserInput());
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
