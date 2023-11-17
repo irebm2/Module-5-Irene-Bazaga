719ccf6b65cb3e856fb4eda6be98c6af35fb82aa
irebm2 <irebazaga@gmail.com>
2023-11-17 20:19:49 +0000

    oki

--- script.js
+++ script.js
@@ -169,14 +169,14 @@ function generatePasswordWithUserInput() {
 }
 
 // Testing our function
-console.log(generatePasswordWithUserInput());
+// console.log(generatePasswordWithUserInput());
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePasswordWithUserInput();
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
